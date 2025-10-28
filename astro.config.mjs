01a86e21312ded57eca9d24312cdd3cfdb7eab11
Damian Zyznar <damian.zyznar@idealogic.pl>
2025-10-28 11:52:26 +0100

    fix packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://damczi17/github.io',
   base: '/osp',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
